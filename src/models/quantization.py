3a3bffa643f9091a9a56ca047d36452cd57e3d3a
DESKTOP-ABB8PBE\pablo <202110501@alu.comillas.edu>
2024-11-23 17:15:08 +0100

    [ADD] Added initial implementation of models interface.

---
+++ src/models/quantization.py
@@ -0,0 +1,35 @@
+import torch
+from transformers import BitsAndBytesConfig
+
+
+class QuantizationLoader:
+
+    def __init__(
+        self,
+        load_in_4bit=True,                    # Enable loading the model in 4-bit precision
+        bnb_4bit_quant_type="nf4",            # Specify quantization type as Normal Float 4
+        bnb_4bit_compute_dtype=getattr(torch, "bfloat16"), # Set computation data type
+        bnb_4bit_use_double_quant=True
+    ) -> None:
+        """
+        load_in_4bit: Enable loading the model in 4-bit precision
+        bnb_4bit_quant_type: Quantization type
+        bnb_4bit_compute_dtype: Computation data type
+        bnb_4bit_use_double_quant: Use double quantization for better accuracy
+        """
+
+        self.load_in_4bit = load_in_4bit
+        self.bnb_4bit_quant_type = bnb_4bit_quant_type
+        self.bnb_4bit_compute_dtype = bnb_4bit_compute_dtype
+        self.bnb_4bit_use_double_quant = bnb_4bit_use_double_quant
+
+    def load_config(self) -> BitsAndBytesConfig:
+        
+        self.config = BitsAndBytesConfig(
+            load_in_4bit=self.load_in_4bit,
+            bnb_4bit_quant_type=self.bnb_4bit_quant_type,
+            bnb_4bit_compute_dtype=self.bnb_4bit_compute_dtype,
+            bnb_4bit_use_double_quant=self.bnb_4bit_use_double_quant
+        )
+
+        return self.config
---
+++ src/models/tokenizers.py
@@ -0,0 +1,29 @@
+from transformers import AutoTokenizer
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+
+class TokenizerLoader:
+
+    def __init__(
+            self,
+            model_name: str,
+            add_eos_token: bool = True,
+            use_fast: bool = True,
+            padding_side: str = 'left'
+        ) -> None:
+        
+        self.model_name = model_name
+        self.add_eos_token = add_eos_token
+        self.use_fast = use_fast
+        self.padding_side = padding_side
+
+    def load(self) -> PreTrainedTokenizerBase:
+
+        self.tokenizer = AutoTokenizer(
+            self.model_name,
+            add_eos_token = self.add_eos_token,
+            use_fast = self.use_fast,
+            padding_side = self.padding_side
+        )
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+        return self.tokenizer
