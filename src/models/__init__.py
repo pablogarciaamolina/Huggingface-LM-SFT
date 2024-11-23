19d7cf4bb68aae6114c868595e7232f0060da470
DESKTOP-ABB8PBE\pablo <202110501@alu.comillas.edu>
2024-11-23 17:33:21 +0100

    [UPDATE] Corrected models and train.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,4 @@
-from .models import *
-from .tokenizers import *
+from .models import ModelLoader, ModelType
+from .tokenizers import TokenizerLoader
+from .quantization import QuantizationLoader
+from .sft import PEFTLoader, LoRALoader, PromptTunningLoader
